e6f363f7f46f5dfcb0c6d72b74914c823d3b8fcb
Daniel <>
2023-03-19 14:59:08 +0000

    Make main.py detect package files & source section

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ from src import build
 from src import install
 
 
+def install_package(definition):
+    if "source" in definition:
+        source = definition["source"]
+        if "http-get" in source:
+            http = source["http-get"]
+            if "hash" in http:
+                fetch.http_download(http["url"], http["hash"])
+            else:
+                print(
+                    """Warning: Fetching source with HTTP
+                    but not verifying integrity with hash"""
+                )
+                fetch.http_download(http["url"])
+
+        elif "git-fetch" in source:
+            git = source["git-fetch"]
+            fetch.git_fetch(git["url"])
+
+        else:
+            sys.exit("Error: malformed source section")
+
+    if "patch" in definition:
+        """TODO apply patches to source"""
+
+    if "build" in definition:
+        """TODO build"""
+
+    if "install" in definition:
+        """TODO install"""
+
+
 if __name__ == "__main__":
     # Program should be run with a config file as argument
     if len(sys.argv) != 2:
@@ -16,17 +47,7 @@ if __name__ == "__main__":
     config = parse.parse_config(config_file)
     config_file.close()
 
-    package_definition = config["package"]["definition"]
-
-    # If a config field is not present, the step is skipped
-    if "source" in package_definition:
-        """TODO fetch source"""
-
-    if "patch" in package_definition:
-        """TODO apply patches to source"""
-
-    if "build" in package_definition:
-        """TODO build"""
-
-    if "install" in package_definition:
-        """TODO install"""
+    if "package" in config:
+        install_package(config["package"]["definition"])
+    else:
+        """TODO handle system and service cases"""
