a540b80ca5e30e82d3bfafd24140e6f0bc629e45
Daniel <>
2023-03-19 15:01:43 +0000

    Create basic run_gnu_build_system()

--- src/build.py
+++ src/build.py
@@ -1,8 +1,16 @@
 import subprocess
+import os
+import sys
 
-# ./configure; make; make install
+
 def run_gnu_build_system():
-    """TODO"""
+    autotools_commands = []
+    if os.path.isfile("configure"):
+        autotools_commands.append("./configure")
+    autotools_commands.append("make")
+
+    for command in autotools_commands:
+        subprocess.run(command, shell=True)
 
 
 # commands as a list of strings to be executed
