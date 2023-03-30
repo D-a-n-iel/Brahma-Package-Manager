8dbb0c95421cea82f0e1542fea80f2016f644806
Daniel <>
2023-03-30 18:34:59 +0100

    Restructure main and Add basic handling for build

--- main.py
+++ main.py
@@ -7,36 +7,59 @@ from src import install
 from src import utils
 
 
-def install_package(definition):
-    print(definition)
-    if "source" in definition:
-        try:
-            source = definition["source"]
-            if "http-get" in source:
-                http = source["http-get"]
-                if "hash" in http:
-                    fetch.http_download(http["url"], http["hash"])
-                else:
-                    print(
-                        """Warning: Fetching source with HTTP
-                        but not verifying integrity with hash"""
-                    )
-                    fetch.http_download(http["url"])
+def fetching_step(source):
+    try:
+        if "http-get" in source:
+            http = source["http-get"]
+            if "hash" in http:
+                archive_name = fetch.http_download(http["url"], http["hash"])
+                return fetch.extract(archive_name)
+            else:
+                print(
+                    """Warning: Fetching source with HTTP
+                    but not verifying integrity with hash"""
+                )
+                archive_name = fetch.http_download(http["url"])
+                return fetch.extract(archive_name)
 
-            elif "git-fetch" in source:
-                git = source["git-fetch"]
-                if "branch" in git:
-                    fetch.git_fetch(git["url"], git["branch"])
-                else:
-                    fetch.git_fetch(git["url"])
-        except KeyError:
-            sys.exit("Error: malformed source section")
+        elif "git-fetch" in source:
+            git = source["git-fetch"]
+            if "branch" in git:
+                return fetch.git_fetch(git["url"], git["branch"])
+            else:
+                return fetch.git_fetch(git["url"])
+    except KeyError:
+        sys.exit("Error: malformed source section")
+
+
+def building_step(build_proc, build_dir):
+    try:
+        with utils.cd(build_dir):
+            if "system" in build_proc:
+                system = build_proc["system"]
+                if system == "gnu_build_system":
+                    print(build_proc)
+                    if "prefix" in build_proc:
+                        build.run_gnu_build_system(build_proc["prefix"])
+                    else:
+                        build.run_gnu_build_system()
+
+            if "commands" in build_proc:
+                commands = build_proc["commands"]
+                build.run_build_commands(commands)
+    except KeyError:
+        sys.exit("Error: malformed build section")
+
+
+def install_package(definition):
+    if "source" in definition:
+        build_dir = fetching_step(definition["source"])
 
     if "patch" in definition:
         """TODO apply patches to source"""
 
     if "build" in definition:
-        """TODO build"""
+        building_step(definition["build"], build_dir)
 
     if "install" in definition:
         """TODO install"""
